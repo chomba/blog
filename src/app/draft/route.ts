57f6dfdadbefc2724321d6f27515af7cac333fae
Juan Chomba <juan@chomba.net>
2025-11-10 21:17:56 -0500

    test

--- src/app/draft/route.ts
+++ src/app/draft/route.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/management";
-import { redirect } from "next/navigation";
+import { permanentRedirect, redirect, RedirectType } from "next/navigation";
 import { draftMode } from "next/headers";
 import { NextRequest } from "next/server";
 
@@ -22,5 +22,7 @@ export async function GET(request: NextRequest) {
     // const draft = await draftMode();
     // draft.enable();
     // console.log(`FULL SLUG: ${post.full_slug}`);
-    redirect(post.full_slug != "home" ? post.full_slug : "/");
+    const redirectTo = post.full_slug != "home" ? `/${post.full_slug}` : "/";
+    permanentRedirect(redirectTo);
+    // permanentRedirect(post.full_slug != "home" ? post.full_slug : "/");
 }

82e2f2cf4d338f6d6d6300f345aa8c886bd59ba9
Juan Chomba <juan@chomba.net>
2025-11-10 22:50:46 -0500

    test

--- src/app/draft/route.ts
+++ src/app/draft/route.ts
@@ -8,21 +8,26 @@ export async function GET(request: NextRequest) {
     const secret = searchParams.get("secret");
     let slug = searchParams.get("slug");
 
-    if (secret != process.env.DRAFT_MODE_SECRET) {
-        return new Response("Invalid Token", { status: 401 });
-    }
+    redirect(`${slug}?secret=${secret}`);
+
+    // const redirectTo = slug === "home" ? `/${slug}` : "/";
+
+
+    // if (secret != process.env.DRAFT_MODE_SECRET) {
+    //     return new Response("Invalid Token", { status: 401 });
+    // }
 
     // if (!slug) {
     //     return new Response("You must provide a slug as a query paramenter", { status: 401 });
     // }
-    const post = slug ? await api.root().draft().slug(slug) : await api.root().draft().slug("home");
-    if (!post) {
-        return new Response("You must provide a valid slug", { status: 401 });
-    }
+    // const post = slug ? await api.root().draft().slug(slug) : await api.root().draft().slug("home");
+    // if (!post) {
+    //     return new Response("You must provide a valid slug", { status: 401 });
+    // }
     // const draft = await draftMode();
     // draft.enable();
     // console.log(`FULL SLUG: ${post.full_slug}`);
-    const redirectTo = post.full_slug != "home" ? `/${post.full_slug}` : "/";
-    permanentRedirect(redirectTo);
+    // const redirectTo = post.full_slug != "home" ? `/${post.full_slug}` : "/";
+    // permanentRedirect(redirectTo);
     // permanentRedirect(post.full_slug != "home" ? post.full_slug : "/");
 }

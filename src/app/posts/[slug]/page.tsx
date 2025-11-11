ed74002e5a84ff60ef34d84c653af890b7b82a92
Juan Chomba <juan@chomba.net>
2025-11-10 19:25:39 -0500

    test

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -10,12 +10,12 @@ export const generateStaticParams = async () => {
 
 export default async function PostPage({ params }: { params: Promise<{ slug: string }>}) {
     const client = getStoryblokApi();
-    const version = await resolveVersion();
+    // const version = await resolveVersion();
 
     const { slug } = await params;
-    const post = await client.getStory(`posts/${slug}`, {
-        version: version
-    });
-    // const post = await api.posts().slug(slug);
-    return <StoryblokStory story={post.data.story} slug={post.data.story.full_slug} />;
+    // const post = await client.getStory(`posts/${slug}`, {
+    //     version: "draft"
+    // });
+    const post = await api.posts().slug(slug);
+    return <StoryblokStory story={post} slug={post.full_slug} />;
 }
